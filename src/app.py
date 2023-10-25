06d7e8212fb3b1050b2b1a7e4dab827676dba5d3
shiyanboxer <shiyanboxer@gmail.com>
2023-10-25 16:53:59 -0400

    Update APIs

--- src/app.py
+++ src/app.py
@@ -4,10 +4,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-
 CORS(app)
 
-# Custom Axios Instance
+# Create a single 'recall' session with common headers
 recall = requests.Session()
 recall.headers.update({
     'authorization': f'token {os.environ["RECALL_API_KEY"]}',
@@ -15,40 +14,77 @@ recall.headers.update({
 })
 recall.timeout = 10
 
-# Gets the bot name and meeting link from the request body
-# Connects the bot to the meeting
+
+# Function to make a generic API call
+def make_recall_api_call(endpoint, method='POST', data=None):
+    try:
+        response = recall.request(
+            method,
+            f'https://api.recall.ai/api/v1/bot/{endpoint}',
+            json=data,
+        )
+        response.raise_for_status()
+        return response.json()
+    except Exception as err:
+        return str(err), 500
 
 
+# Establish connection with meeting and bot
 @app.route('/', methods=['POST'])
 def connect_bot():
-    my_request = request.json
-    bot_name = my_request.get('bot_name')
-    meeting_url = my_request.get('meeting_url')
+    data = request.json
+    bot_name = data.get('bot_name')
+    meeting_url = data.get('meeting_url')
 
     if not bot_name or not meeting_url:
         return jsonify({'error': 'Bot name and meeting URL are required'}), 400
-    try:
-        payload = {
-            'meeting_url': meeting_url,
-            'bot_name': bot_name,
-            'transcription_options': {'provider': 'assembly_ai'},
-            'real_time_transcription': {'destination_url': f'{os.environ["WEBHOOK_URL"]}/meeting_transcript'}
-        }
-        response = recall.post(
-            'https://api.recall.ai/api/v1/bot', json=payload)
-        data = response.json()
-        print(data)
-        return jsonify({'id': data['id']}), 200
-    except Exception as err:
-        return jsonify({'error': str(err)}), 500
+
+    payload = {
+        'meeting_url': meeting_url,
+        'bot_name': bot_name,
+        'transcription_options': {'provider': 'assembly_ai'},
+        'real_time_transcription': {'destination_url': f'{os.environ["WEBHOOK_URL"]}/meeting_transcript'}
+    }
+    result = make_recall_api_call('bot', 'POST', payload)
+    return jsonify(result), 200
 
 
-def leave_meeting(meeting_id):
-    try:
-        recall.post(
-            f'https://api.recall.ai/api/v1/bot/{meeting_id}/leave_call')
-    except Exception as err:
-        print(err)
+# Disconnect bot from meeting
+@app.route('/disconnect', methods=['POST'])
+def leave_meeting():
+    data = request.json
+    meeting_id = data.get('meetingId')
+
+    if not meeting_id:
+        return jsonify({'error': 'Meeting ID is required'}), 400
+
+    result = make_recall_api_call(f'bot/{meeting_id}/leave_call')
+    return jsonify(result), 200
+
+# Pause meeting recording
+
+
+@app.route('/pause', methods=['POST'])
+def pause_recording():
+    data = request.json
+    meeting_id = data.get('meetingId')
+    if not meeting_id:
+        return jsonify({'error': 'Meeting ID is required'}), 400
+
+    result = make_recall_api_call(f'bot/{meeting_id}/pause_recording')
+    return jsonify(result), 200
+
+
+# Resume meeting recording
+@app.route('/resume', methods=['POST'])
+def resume_recording():
+    data = request.json
+    meeting_id = data.get('meetingId')
+    if not meeting_id:
+        return jsonify({'error': 'Meeting ID is required'}), 400
+
+    result = make_recall_api_call(f'bot/{meeting_id}/resume_recording')
+    return jsonify(result), 200
 
 
 if __name__ == '__main__':
