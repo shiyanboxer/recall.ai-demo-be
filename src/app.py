05c77100fe44d611c63027ab1c747a7df167a0fa
shiyanboxer <shiyanboxer@gmail.com>
2023-10-24 21:32:43 -0400

    Test

--- src/app.py
+++ src/app.py
@@ -17,12 +17,14 @@ recall.timeout = 10
 
 # Gets the bot name and meeting link from the request body
 # Connects the bot to the meeting
+
+
 @app.route('/', methods=['POST'])
-def connect_bot():    
+def connect_bot():
     my_request = request.json
     bot_name = my_request.get('bot_name')
     meeting_url = my_request.get('meeting_url')
-    
+
     if not bot_name or not meeting_url:
         return jsonify({'error': 'Bot name and meeting URL are required'}), 400
     try:
@@ -32,18 +34,22 @@ def connect_bot():
             'transcription_options': {'provider': 'assembly_ai'},
             'real_time_transcription': {'destination_url': f'{os.environ["WEBHOOK_URL"]}/meeting_transcript'}
         }
-        response = recall.post('https://api.recall.ai/api/v1/bot', json=payload)
+        response = recall.post(
+            'https://api.recall.ai/api/v1/bot', json=payload)
         data = response.json()
         print(data)
         return jsonify({'id': data['id']}), 200
     except Exception as err:
         return jsonify({'error': str(err)}), 500
 
+
 def leave_meeting(meeting_id):
     try:
-        recall.post(f'https://api.recall.ai/api/v1/bot/{meeting_id}/leave_call')
+        recall.post(
+            f'https://api.recall.ai/api/v1/bot/{meeting_id}/leave_call')
     except Exception as err:
         print(err)
+
 
 if __name__ == '__main__':
     app.run(port=5000)
